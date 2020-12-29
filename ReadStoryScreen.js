fad9e6eb4dcabb96453bd36ae67340cb54008815
ShanykaBhandary <69717003+ShanykaBhandary@users.noreply.github.com>
2020-12-29 17:35:45 +0530

    Add files via upload

---
+++ ReadStoryScreen.js
@@ -0,0 +1,10 @@
+import { NativeAppEventEmitter } from "react-native";
+import * as React from 'react'
+import {Text,TouchableOpacity,Button,View} from 'react-native'
+export default class ReadStoryScreen extends React.Component{
+    render(){
+        return(
+            <Text>WELCOME TO READ STORY SCREEN</Text>
+        )
+    }
+}
